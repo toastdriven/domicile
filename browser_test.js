eb40c876633f3583c4ad0f7cd21f56404a6e5be3
Daniel Lindsley <daniel@toastdriven.com>
2014-09-03 11:29:39 -0500

    A better in-browser test.

--- browser_test.js
+++ browser_test.js
@@ -7,16 +7,18 @@ var login = new Element('form', {method: 'POST', action: '#'}
 )
 
 var alt = D.form({method: 'POST', action: '#'}
-  , D.input({'type': 'radio', 'value': 'yes'}, "Yes")
-  , D.input({'type': 'radio', 'value': 'no'}, "No")
+  , D.input({'type': 'radio', 'name': 'poll', 'value': 'yes', 'checked': 'checked'}, "Yes")
+  , D.input({'type': 'radio', 'name': 'poll', 'value': 'no'}, "No")
   , D.input({'type': 'submit', 'value': 'Like it?'})
 )
 
 document.onreadystatechange = function() {
-  var via_inner = document.getElementById('via_innerHTML')
-  via_inner.innerHTML = login.render_html()
+  if(document.readyState === 'complete') {
+    var via_inner = document.getElementById('via_innerHTML')
+    via_inner.innerHTML = login.render_html()
 
-  var via_dom = document.getElementById('via_dom')
-  var like_form = alt.render_dom()
-  via_dom.appendChild(like_form)
+    var via_dom = document.getElementById('via_dom')
+    var like_form = alt.render_dom()
+    via_dom.appendChild(like_form)
+  }
 }
